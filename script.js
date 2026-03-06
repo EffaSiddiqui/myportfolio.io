1698500924fb15c0866e1d2040074f0836882353
Effa Siddiqui <162863166+EffaSiddiqui@users.noreply.github.com>
2026-03-06 19:25:48 +0500

    script.js

--- script.js
+++ script.js
@@ -1,31 +1,21 @@
 document.addEventListener("DOMContentLoaded",()=>{
 
-const navLinks=document.querySelectorAll(".nav-links li")
-const pages=document.querySelectorAll(".content-tab")
+  const navLinks=document.querySelectorAll(".nav-links li")
+  const pages=document.querySelectorAll(".content-tab")
 
-navLinks.forEach(link=>{
-
-link.addEventListener("click",()=>{
-
-const targetPage=link.getAttribute("data-page")
-
-navLinks.forEach(l=>l.classList.remove("active-tab"))
-link.classList.add("active-tab")
-
-pages.forEach(page=>{
-
-page.classList.remove("current")
-
-if(page.id===targetPage){
-page.classList.add("current")
-}
-
-})
-
-window.scrollTo({top:0,behavior:"smooth"})
-
-})
-
-})
+  navLinks.forEach(link=>{
+    link.addEventListener("click",()=>{
+      const targetPage=link.getAttribute("data-page")
+      navLinks.forEach(l=>l.classList.remove("active-tab"))
+      link.classList.add("active-tab")
+      pages.forEach(page=>{
+        page.classList.remove("current")
+        if(page.id===targetPage){
+          page.classList.add("current")
+        }
+      })
+      window.scrollTo({top:0,behavior:"smooth"})
+    })
+  })
 
 })
